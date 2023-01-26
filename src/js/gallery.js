1a30b6bb2c60eaf7cf9fe75ef511b595cdff7aca
Andrey Obibok <andreyobibok@gmail.com>
2023-01-26 16:57:09 +0200

    fetch trending movies is now using a class

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,15 +1,36 @@
 import { createMarkup } from "./createMarkup";
-const API_URL_POPULAR =
-    "https://api.themoviedb.org/3/trending/movie/week?api_key=6b1b36ecf2f3f3c0d27307e18cbffcb3&page=1";
+import ApiService from './apiService';
 
-getMovies(API_URL_POPULAR);
+const apiService = new ApiService();
 
-async function getMovies(url) {
-  const resp = await fetch(url);
-    const respData = await resp.json();
-    createMarkup(respData)
-    const movie = respData.results
-    localStorage.setItem('movie', JSON.stringify(movie))
-    const localMovie = JSON.parse(localStorage.getItem('Movie'));
-    return respData
+getMovies();
+
+async function getMovies() {
+  const response = await apiService.fetchTrendFilms();
+  const results = response.data.results;
+  createMarkup(results);
 }
+
+// getMovies(respData)
+
+// async function getMovies(data) {
+//   const response = await data.json();
+//   createMarkup(response);
+
+// }
+
+
+// const API_URL_POPULAR =
+//     "https://api.themoviedb.org/3/trending/movie/week?api_key=6b1b36ecf2f3f3c0d27307e18cbffcb3&page=1";
+
+// getMovies(API_URL_POPULAR);
+
+// async function getMovies(url) {
+//   const resp = await fetch(url);
+//     const respData = await resp.json();
+//     createMarkup(respData)
+//     const movie = respData.results
+//     localStorage.setItem('movie', JSON.stringify(movie))
+//     const localMovie = JSON.parse(localStorage.getItem('Movie'));
+//     return respData
+// }
