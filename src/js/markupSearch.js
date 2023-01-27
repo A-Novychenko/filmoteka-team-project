83d41d694772f510dd8fe6e352aef5ade6f5b24d
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-01-27 20:22:26 +0200

    feature/team-modal-upd

--- src/js/markupSearch.js
+++ src/js/markupSearch.js
@@ -1,26 +1,24 @@
-const gallery = document.querySelector('.js-movies-list')
-const IMG_URL = 'https://image.tmdb.org/t/p/w500/'
+const gallery = document.querySelector('.js-movies-list');
+const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
 
-export  function renderMarkupSearch(movies) {
-    // console.log(movies);
-    const markup = movies.map(movie => {
-        // console.log(movie.id);
-       return  `<li class="movie__item" data-movie="${movie.id}">
+export function renderMarkupSearch(movies) {
+  // console.log(movies);
+  const markup = movies
+    .map(movie => {
+      // console.log(movie.id);
+      return `<li class="movie__item" data-movie="${movie.id}">
         <img src="${IMG_URL + movie.poster_path}" alt="${movie.original_title}">
                 <h3 class="card_title">${movie.title}</h3>
-                    <p class="card_genres">${ Object.values(movie.genre_ids)
-            }</p>
-                    <p class="card_release_date">${movie.release_date
-            }</p>
+                    <p class="card_genres">${Object.values(movie.genre_ids)}</p>
+                    <p class="card_release_date">${movie.release_date}</p>
                 </li>
             `;
-    }).join(''); 
+    })
+    .join('');
 
-  return markup
+  return markup;
 }
 
 export function cleanHtml() {
   gallery.innerHTML = '';
-  
 }
- 
