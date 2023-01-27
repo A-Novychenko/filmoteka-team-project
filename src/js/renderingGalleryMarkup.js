603b36755b79186ab5f23c6303ee84e8936280a8
Romankk7 <klymenkoromca18@gmail.com>
2023-01-27 15:41:44 +0200

    змінив try catch

--- src/js/renderingGalleryMarkup.js
+++ src/js/renderingGalleryMarkup.js
@@ -6,10 +6,9 @@ const apiService = new ApiService();
 getMovies();
 
 export async function getMovies() {
+  try {
   const response = await apiService.fetchTrendFilms();
   const results = response.data.results;
-  console.log(results)
-  try {
     movieContainer.innerHTML = renderMarkupSearch(results)
   } catch (err) {
     console.log(err)
