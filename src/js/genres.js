903bcbafff413e0e52e5965b9833cf397ae26ec9
VitaliySlivchuk <41226137+VitaliySlivchuk@users.noreply.github.com>
2023-01-29 21:07:24 +0200

    first
    створені функції
    1. Перевірка чи є жанри фільма
    2. Переписав перевірку на кількість жанрів фінкією

--- src/js/genres.js
+++ src/js/genres.js
@@ -8,4 +8,16 @@ function findGenres(genersArr, localArr, arr) {
   });
 }
 
-export { findGenres, getGenres };
+function isEmptyGanres(arr) {
+  if (arr.length === 0) {
+    arr.push(['No data']);
+  }
+}
+
+function isMoreThenTwoGanres(arr) {
+  if (arr.length > 3) {
+    arr.splice(2, arr.length - 2, 'Other');
+  }
+}
+
+export { findGenres, isEmptyGanres, isMoreThenTwoGanres };
