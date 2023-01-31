bde703f32427c46995e99eee61d1d40c2874e735
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-01-31 16:14:27 +0200

    tt

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 import { headerForm, errorSearch } from './js/refs';
 import { onHeaderFormClick } from './js/searchByKeyWord';
 import * as trailer from './js/trailer';
-​
+
 headerForm.addEventListener('submit', onHeaderFormClick);
-​
+
 import { oneToggle } from './js/modal-movie';
 import { closeBtn } from './js/modal-movie';
 import { keyPressEscCloseMovieModal } from './js/modal-movie';
@@ -13,25 +13,25 @@ import { renderMarkupModal } from './js/markupModal';
 // // тестую роботу ApiService:
 import ApiService from './js/apiService';
 import onChangeTheme from './js/theme-switch';
-​
+
 // theme-switch.js
-​
+
 // const service = new ApiService();
 // //популярні:
-​
+
 // // const service = new ApiService();
 // // // //популярні:
-​
+
 // // service.fetchTrendFilms();
 // //жанри:
 // console.log(service.fetchGenres());
-​
+
 // //по ключовому слову:
 // // service.query = 'cat';
 // service.fetchFilmsByKeyWord();
-​
+
 import { getMovies } from './js/renderingGalleryMarkup';
-​
+
 /// ////////////footer-modal
 import { openModalTeamBtn, closeModalTeamBtn, modalTeam } from './js/refs';
 import {
@@ -39,14 +39,14 @@ import {
   toggleTeamModal,
   clickBackdropCloseTeamModal,
 } from './js/team-modal';
-​
+
 openModalTeamBtn.addEventListener('click', toggleTeamModal);
 closeModalTeamBtn.addEventListener('click', toggleTeamModal);
 modalTeam.addEventListener('click', clickBackdropCloseTeamModal);
 //////////////// footer-modal
-​
+
 // feature/modal-btns-library
 import { onClick } from './js/modal-btns-library';
-​
+
 // сетить жанри в локальне сховище
 import './js/getGenres';
