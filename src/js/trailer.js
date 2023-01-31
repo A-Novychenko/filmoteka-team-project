b56801869ca8694c004b42c33b34547fe64ef397
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-01-31 18:47:35 +0200

    ggg

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,27 +1,30 @@
 import axios from 'axios';
 import { modalGallery } from './refs';
 
-const modalGallery = document.querySelector('.movie_modal_gallery');
+// const modalGallery = document.querySelector('.movie_modal_gallery');
 
 modalGallery.addEventListener('click', onBtnTrailer);
 let idTrailer = '';
 
 async function onBtnTrailer(evt) {
-    try {
-        const btnTrailer = evt.target.closest('.js-btn_trailer');
-        console.log(btnTrailer)
-        idTrailer = btnTrailer.dataset.idmovie;
+  try {
+    const btnTrailer = evt.target.closest('.js-btn_trailer');
+    console.log(btnTrailer);
+    idTrailer = btnTrailer.dataset.idmovie;
 
-        const resp = await fetchTrailer();
-        const dataMovie = resp.data.results;
-        const officialTrailer = dataMovie.find(movie =>
-            movie.name.toLowerCase().includes('official trailer') || movie.name.toLowerCase().includes('official trailer 2'));
-        
-        // console.log(officialTrailer)
-        
-        if (officialTrailer) {
-            // console.log('є трейлер!!!')
-            modalGallery.innerHTML = `<iframe 
+    const resp = await fetchTrailer();
+    const dataMovie = resp.data.results;
+    const officialTrailer = dataMovie.find(
+      movie =>
+        movie.name.toLowerCase().includes('official trailer') ||
+        movie.name.toLowerCase().includes('official trailer 2')
+    );
+
+    // console.log(officialTrailer)
+
+    if (officialTrailer) {
+      // console.log('є трейлер!!!')
+      modalGallery.innerHTML = `<iframe 
             class="movie_trailer"
             width='700'
             height='500'
@@ -36,25 +39,22 @@ async function onBtnTrailer(evt) {
             web-share"
             allowfullscreen>
             </iframe>`;
-            
-        } else {
-            // console.log('немає трейлера(((');
-            btnTrailer.innerHTML = `<p class="error__trailer">Sorry, search result not successful</p>`
-        }
-    } catch (err) {
-        console.log(err)
+    } else {
+      // console.log('немає трейлера(((');
+      btnTrailer.innerHTML = `<p class="error__trailer">Sorry, search result not successful</p>`;
     }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-
-
 async function fetchTrailer() {
-    try {
-        const URL = `https://api.themoviedb.org/3/movie/${idTrailer}/videos?api_key=6b1b36ecf2f3f3c0d27307e18cbffcb3&language=en-US`;
-        const response = await axios.get(URL);
+  try {
+    const URL = `https://api.themoviedb.org/3/movie/${idTrailer}/videos?api_key=6b1b36ecf2f3f3c0d27307e18cbffcb3&language=en-US`;
+    const response = await axios.get(URL);
 
-        return response;  
-    } catch (error) {
-        console.log(error.message)
-    }
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
