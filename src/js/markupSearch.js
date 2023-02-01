f2b4d5d62f8ed1869e4f27c538d2bfd5e46c2693
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-02-01 11:06:17 +0200

    почистив консоль

--- src/js/markupSearch.js
+++ src/js/markupSearch.js
@@ -11,7 +11,7 @@ import noPoster from '../images/no-poster.jpg';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
 
 export function renderMarkupSearch(movies) {
-  console.log('movies: ', movies);
+  // console.log('movies: ', movies);
   const markup = movies
     .map(movie => {
       const { genre_ids } = movie;
